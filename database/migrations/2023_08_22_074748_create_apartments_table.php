eb03e3428b55b1055cf86a7eee02870199b84404
andriy-kolokolov <koloandre22@gmail..com>
2023-08-22 09:59:40 +0200

    add migrations

---
+++ database/migrations/2023_08_22_074748_create_apartments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name');
+            $table->integer('rooms');
+            $table->integer('beds');
+            $table->integer('bathrooms');
+            $table->integer('square_meters');
+            $table->json('images');
+            $table->boolean('is_available');
+            $table->boolean('sponsor');
+            $table->string('zip');
+            $table->string('city');
+            $table->string('address');
+            $table->string('gps_coordinates');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
