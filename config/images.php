1d46d02477761a56b13df09706ca402c8629eec5
Antonio D'Angelo <reapercsp@gmail.com>
2023-08-25 00:57:18 +0200

    array structure

---
+++ config/images.php
@@ -0,0 +1,254 @@
+<?php
+
+return [
+    [
+        'apartment_id' => '1',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '2',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '3',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '4',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '5',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '6',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '7',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '8',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '9',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '10',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '11',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '12',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '13',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '14',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '15',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '16',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '17',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '18',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '19',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '20',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '21',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '22',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '23',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '24',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '25',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '26',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '27',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '28',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '29',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '30',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '31',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '32',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '33',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '34',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '35',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '36',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '37',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '38',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '39',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '40',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '41',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '42',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '43',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '44',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '45',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '46',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '47',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '48',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '49',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+    [
+        'apartment_id' => '50',
+        'is_cover'     => '',
+        'image_path'   => '',
+    ],
+];
