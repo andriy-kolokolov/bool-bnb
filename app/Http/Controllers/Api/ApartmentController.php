776fc992e66fd3e6669bb43b77df775aedad3c07
andriy-kolokolov <koloandre22@gmail.com>
2023-08-26 13:54:07 +0200

    done update method for apartment

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Address;
 use App\Models\Apartment;
 use App\Models\Image;
 use App\Models\Service;
@@ -10,18 +11,19 @@ use App\Models\Sponsorship;
 use App\Models\View;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class ApartmentController extends Controller {
     // START CRUDS
 
     public function index(): JsonResponse {
-        $apartments = Apartment::with(['user', 'addresses', 'services', 'images', 'views'])
+        $apartments = Apartment::with(['user', 'address', 'services', 'images', 'views'])
             ->get();
         return response()->json($apartments);
     }
 
     public function show($id): JsonResponse {
-        $apartment = Apartment::with(['user', 'addresses', 'services', 'images', 'views'])
+        $apartment = Apartment::with(['user', 'address', 'services', 'images', 'views'])
             ->find($id);
         if (!$apartment) {
             return response()->json(['message' => 'Apartment not found'], 404);
@@ -30,7 +32,7 @@ class ApartmentController extends Controller {
     }
 
     public function store(Request $request): JsonResponse {
-        {
+        try {
             $validatedData = $request->validate([
                 'name' => 'required|string|max:200',
                 'rooms' => 'required|integer',
@@ -40,12 +42,12 @@ class ApartmentController extends Controller {
                 'is_available' => 'required|boolean',
                 'is_sponsored' => 'required|boolean',
                 'user.id' => 'required|exists:users,id',
-                'addresses' => 'required|array',
-                'addresses.*.street' => 'required|string|max:200',
-                'addresses.*.city' => 'required|string|max:100',
-                'addresses.*.zip' => 'required|string|max:10',
-                //                'addresses.*.latitude' => 'string',
-                //                'addresses.*.longitude' => 'string',
+                'address' => 'required|array',
+                'address.*.street' => 'required|string|max:200',
+                'address.*.city' => 'required|string|max:100',
+                'address.*.zip' => 'required|string|max:10',
+                //                'address.*.latitude' => 'string',
+                //                'address.*.longitude' => 'string',
                 'services' => 'array',
                 'services.*.name' => 'required|max:70',
                 'services.*.icon' => 'max:100',
@@ -70,7 +72,7 @@ class ApartmentController extends Controller {
             $apartment->user()->associate($user); // Associate user
             $apartment->save();
 
-            $addressesData = $validatedData['addresses'];
+            $addressesData = $validatedData['address'];
             $apartment->address()->createMany($addressesData);
 
             if (isset($validatedData['services'])) {
@@ -123,90 +125,114 @@ class ApartmentController extends Controller {
             }
 
             return response()->json(['message' => 'Apartment created successfully']);
+        } catch (ValidationException $e) {
+            return response()->json(['errors' => $e->errors()], 422);
         }
     }
 
-    public function update(Request $request, $id): JsonResponse {
-        $apartment = Apartment::findOrFail($id);
+    public function update(Request $request, int $id) {
+        try {
+            $apartment = Apartment::find($id);
+            $validatedData = $request->validate([
+                'name' => 'string|max:200',
+                'rooms' => 'integer',
+                'beds' => 'integer',
+                'bathrooms' => 'integer',
+                'square_meters' => 'integer',
+                'is_available' => 'boolean',
+                'is_sponsored' => 'boolean',
+                'sponsorship_id' => 'nullable|integer',
+                'address' => 'array',
+                'address.*.street' => 'required|string|max:200',
+                'address.*.city' => 'required|string|max:100',
+                'address.*.zip' => 'required|string|max:10',
+                'services' => 'array',
+                'images' => 'array',
+            ]);
 
-        $validatedData = $request->validate([
-            'name' => 'string|max:200',
-            'rooms' => 'integer',
-            'beds' => 'integer',
-            'bathrooms' => 'integer',
-            'square_meters' => 'integer',
-            'is_available' => 'boolean',
-            'is_sponsored' => 'boolean',
-            'user' => 'array', // User data validation
-            'user.id' => 'exists:users,id',
-            'addresses' => 'array', // Addresses data validation
-            'addresses.*.street' => 'string',
-            'addresses.*.city' => 'string',
-            // ... Add more validation rules for other nested data
-        ]);
+            // Update apartment details if sent in request
+            $apartment->update([
+                'name' => $validatedData['name'] ?? $apartment->name,
+                'rooms' => $validatedData['rooms'] ?? $apartment->rooms,
+                'beds' => $validatedData['beds'] ?? $apartment->beds,
+                'bathrooms' => $validatedData['bathrooms'] ?? $apartment->bathrooms,
+                'square_meters' => $validatedData['square_meters'] ?? $apartment->square_meters,
+                'is_available' => $validatedData['is_available'] ?? $apartment->is_available,
+                'is_sponsored' => $validatedData['is_sponsored'] ?? $apartment->is_sponsored,
+            ]);
+            //        dd($request->input(['address']));
+            if ($request->has('address')) {
+                $addressesData = $request->input('address');
+                $address = [];
 
-        $apartment->update([
-            'name' => $validatedData['name'],
-            'rooms' => $validatedData['rooms'],
-            'beds' => $validatedData['beds'],
-            'bathrooms' => $validatedData['bathrooms'],
-            'square_meters' => $validatedData['square_meters'],
-            'is_available' => $validatedData['is_available'],
-            'is_sponsored' => $validatedData['is_sponsored'],
-        ]);
-
-        if (isset($validatedData['user'])) {
-            $user = $validatedData['user']['id'];
-            $apartment->user()->associate($user); // Associate user
-            $apartment->save();
-        }
-
-        if (isset($validatedData['addresses'])) {
-            $addressesData = $validatedData['addresses'];
-            $apartment->addresses()->delete(); // Delete existing addresses
-            $apartment->addresses()->createMany($addressesData); // Create addresses
-        }
-
-        if (isset($validatedData['services'])) {
-            $servicesData = $validatedData['services'];
-            $apartment->services()->sync($servicesData); // Sync services
-        }
-
-        if (isset($validatedData['images'])) {
-            $imagesData = $validatedData['images'];
-            $apartment->images()->delete(); // Delete existing images
-            $images = [];
-
-            foreach ($imagesData as $imageData) {
-                $image = new Image([
-                    'image_path' => $imageData['image_path'],
-                    'is_cover' => $imageData['is_cover'],
-                ]);
-
-                $images[] = $image;
+                foreach ($addressesData as $addressData) {
+                    $address[] = new Address([
+                        'street' => $addressData['street'],
+                        'city' => $addressData['city'],
+                        'zip' => $addressData['zip'],
+                    ]);
+                }
+                $apartment->address()->delete();
+                $apartment->address()->saveMany($address);
             }
 
-            $apartment->images()->saveMany($images);
-        }
+            if ($request->has('services')) {
+                $serviceIds = [];
 
-        if (isset($validatedData['views'])) {
-            $viewsData = $validatedData['views'];
-            $apartment->views()->delete(); // Delete existing views
-            $views = [];
+                foreach ($validatedData['services'] as $serviceData) {
+                    $existingService = Service::where('name', $serviceData['name'])->first();
 
-            foreach ($viewsData as $viewData) {
-                $view = new View([
-                    'ip' => $viewData['ip'],
-                    'date_time' => $viewData['date_time'],
-                ]);
+                    if ($existingService) {
+                        $serviceIds[] = $existingService->id;
+                    } else {
+                        $service = new Service([
+                            'name' => $serviceData['name'],
+                            'icon' => $serviceData['icon'] ?? null,
+                        ]);
 
-                $views[] = $view;
+                        $service->save();
+                        $serviceIds[] = $service->id;
+                    }
+                }
+
+                $apartment->services()->sync($serviceIds);
             }
 
-            $apartment->views()->saveMany($views);
-        }
+            if ($request->has('images')) {
+                $imageData = $request->input('images');
+                $images = [];
 
-        return response()->json(['message' => 'Apartment updated successfully']);
+                foreach ($imageData as $imageInfo) {
+                    $images[] = new Image([
+                        'image_path' => $imageInfo['image_path'],
+                        'is_cover' => $imageInfo['is_cover'],
+                    ]);
+                }
+
+                $apartment->images()->delete();
+                $apartment->images()->saveMany($images);
+            }
+
+            if ($request->has('sponsorship_id')) {
+                $sponsorshipId = $request->input('sponsorship_id');
+
+                if ($sponsorshipId !== null) {
+                    $sponsorship = Sponsorship::findOrFail($sponsorshipId);
+
+                    $apartment->sponsorships()->sync([$sponsorship->id => [
+                        'init_date' => now(),
+                        'end_date' => now()->addDays($sponsorship->duration),
+                    ]]);
+                } else {
+                    $apartment->sponsorships()->detach(); // Remove the relationship
+                }
+            }
+
+            $apartment->update($validatedData); // Update main apartment details
+            return response()->json(['message' => 'Apartment updated successfully']);
+        } catch (ValidationException $e) {
+            return response()->json(['errors' => $e->errors()], 422);
+        }
     }
 
     public function destroy($id): JsonResponse {
